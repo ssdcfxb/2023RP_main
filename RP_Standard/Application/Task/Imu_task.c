49b25d49a156fe862ea47b8356f7938b29a5785e
ssdcfxb <778911093@qq.com>
2022-09-26 23:10:30 +0800

    2022/9/26

--- RP_Standard/Application/Task/Imu_task.c
+++ RP_Standard/Application/Task/Imu_task.c
@@ -8,7 +8,6 @@ void Start_imu_task(void const * argument)
 	
 	for(;;)
 	{
-		LED_GREEN_ON();
 		BMI_Get_RawData(&gyrox, &gyroy, &gyroz, &accx, &accy, &accz);
 		Filter_out = 0.1f * accx + 0.9f * Filter_out;
 		osDelay(1);
